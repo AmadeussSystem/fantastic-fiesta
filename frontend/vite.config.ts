e0df7672b9764a7802c0191b27c9863ce930ab5d
Ayush Saroj <amadeussystem4@example.com>
2025-11-26 23:21:55 +0530

    Auto-update handwritten notes

---
+++ frontend/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+import UnoCSS from 'unocss/vite';
+
+export default defineConfig({
+  plugins: [
+    UnoCSS(),
+    solidPlugin(),
+  ],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: 'esnext',
+  },
+});
