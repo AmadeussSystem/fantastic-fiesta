0e0ad97cf5bcd5ff9fef971364be72fd6a18e25d
Ayush Saroj <amadeussystem04@gmail.com>
2026-01-17 17:40:14 +0530

    Add scribble PNG files and update batch script

--- launch_scrble_with_sync.bat
+++ launch_scrble_with_sync.bat
@@ -3,6 +3,6 @@ REM Launch Scrble Ink
 start "" "shell:AppsFolder\10511ClaudiaWey.inknotespro_9n0kwq747cbjy!App"
 
 REM Launch monitor with correct working directory and log output
-start "" /D "C:\Users\lone\Documents\fantastic-fiesta\dist" monitor_scrble_sync.exe > "C:\Users\lone\Documents\fantastic-fiesta\monitor.log" 2>&1
+start "" /D "C:\Users\FSOS\Documents\fantastic-fiesta\dist" monitor_scrble_sync.exe > "C:\Users\FSOS\Documents\fantastic-fiesta\monitor.log" 2>&1
 
 REM Optional: pause  # Uncomment for testing to keep window open
